2ff2281755a67122d5102dac9fbc11346168aaec
ptonini <pedro.tonini@hotmail.com>
2023-12-04 18:02:08 -0300

    fix: removed outputs

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,4 @@
 output "this" {
-  value = aws_vpc.this
+  value = aws_vpc_endpoint.this
 }
 
-output "default_security_group_id" {
-  value = aws_default_security_group.this.id
-}
-
-output "public_subnet_ids" {
-  value = module.public_subnets[*].this.id
-}
-
-output "private_subnet_ids" {
-  value = module.private_subnets[*].this.id
-}
-
-output "isolated_subnet_ids" {
-  value = module.isolated_subnets[*].this.id
-}
-
-output "peering_requests" {
-  value = aws_vpc_peering_connection.this
-}
